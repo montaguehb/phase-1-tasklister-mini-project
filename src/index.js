823e194e35e10a71b53f093868af253a63cc44e2
Historia Montague <historia.montague@outlook.com>
2023-04-13 09:06:41 -0600

    Finished Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", (event) => {
+    event.preventDefault()
+    addFormInfo(event.target["new-task-description"].value)
+  })
 });
+
+const addFormInfo = (text) => {
+  let li = document.createElement('li');
+  li.textContent = text;
+  document.querySelector('#tasks').appendChild(li);
+}
